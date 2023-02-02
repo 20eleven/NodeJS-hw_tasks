9eb88d2dfaadb58ddec10c8fd0e1076e163649ba
20eleven <pashawh@gmail.com>
2023-02-03 01:22:21 +0300

    add transaction to addUsersToGroupData

--- src/data-access/addUsersToGroupData.ts
+++ src/data-access/addUsersToGroupData.ts
@@ -1,6 +1,10 @@
+import { Transaction } from 'sequelize';
+import db from '../models';
 import { GroupModelType } from '../types/groups';
 import { UserModelType } from '../types/users';
 
 export const addUsersToGroupData = (group: GroupModelType, user: UserModelType) => {
-    return group.addUser(user);
+    db.sequelize.transaction(async (transaction: Transaction) => {
+        await group.addUser(user, { transaction });
+    });
 };
